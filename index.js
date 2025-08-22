298f3334f480b7934490d9106640141777df4a2d
Thang Nguyen <minhthang0403@gmail.com>
2025-08-22 13:48:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import puppeteer from "puppeteer-core";
-import chromium from "chromium";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
+puppeteer.use(StealthPlugin());
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,11 +17,18 @@ app.get("/scrape", async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: true,
-      executablePath: chromium.path, // Dùng binary chromium
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
     const page = await browser.newPage();
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+      "(KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+    );
+    
+    await page.setExtraHTTPHeaders({
+      "accept-language": "en-US,en;q=0.9",
+    });
     await page.goto(url, { waitUntil: "networkidle2" });
 
     const html = await page.content();
