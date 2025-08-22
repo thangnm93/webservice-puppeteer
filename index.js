b76c6910171f4879026f99273098f0177e555ee1
Thang Nguyen <minhthang0403@gmail.com>
2025-08-22 15:18:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ app.get("/scrape", async (req, res) => {
 
     const page = await browser.newPage();
     await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
-      "(KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
     );
     
     await page.setExtraHTTPHeaders({
